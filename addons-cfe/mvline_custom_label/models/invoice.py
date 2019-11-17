f515dddfaddc061a24b1062794e552fd2455f807
Fepo <lferreira@proyectasot.com>
2019-11-16 22:11:39 -0300

    Fileds correctly renamed
    The fields where correctly renamed to fe_DocNro and fe_Serie

--- addons-cfe/mvline_custom_label/models/invoice.py
+++ addons-cfe/mvline_custom_label/models/invoice.py
@@ -9,8 +9,8 @@ class AccountInvoice(models.Model):
     def invoice_validate(self):
         res = super(AccountInvoice, self).invoice_validate()
         for invoice in self:
-            if invoice.x_Serie and invoice.x_DocNro:
-                invoice.number = str(invoice.x_Serie) + '-' + str(invoice.x_DocNro)
+            if invoice.fe_Serie and invoice.fe_DocNro:
+                invoice.number = str(invoice.fe_Serie) + '-' + str(invoice.fe_DocNro)
                 invoice.reference = invoice._get_computed_reference()
                 invoice.move_name = invoice.number
                 invoice.move_id.name = invoice.number
